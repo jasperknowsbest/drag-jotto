707a9823c7aa638561ee942cd2daebc95536d593
Edward Jasper <ejasper@renttherunway.com>
2019-07-11 18:39:20 -0400

    install redux-thunk

--- tests/testUtils.js
+++ tests/testUtils.js
@@ -1,4 +1,22 @@
 import checkPropTypes from "check-prop-types";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../src/reducers";
+import { middlewares } from "../src/configureStore";
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ *  globals: rootReducer, middlewares.
+ * @param {object} initialState - Initial state for store.
+ * @function storeFactory
+ * @returns {Store} - Redux store.
+ */
+
+export const storeFactory = initialState => {
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
+};
 
 /**
  * Return ShallowWrapper containing node(s) with the giving data-test value.
