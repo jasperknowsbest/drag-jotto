cd20991191817e09cc7c21992d21ef587512585f
Edward Jasper <ejasper@renttherunway.com>
2019-07-11 19:11:20 -0400

    add test and code for guessedWord action

--- src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from "./index.js";
-
-describe("correctGuess Action", () => {
-  test("returns an action with the type `CORRECT_GUESS`", () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
-});
